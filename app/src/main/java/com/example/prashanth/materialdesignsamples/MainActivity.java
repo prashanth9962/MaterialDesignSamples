a0f4d29f3dcbe85a526e61e16d15e2fee75566de
Sathvi <sathvisiva@gmail.com>
2015-04-21 00:48:56 +0530

    Navigation drawer

--- app/src/main/java/com/example/prashanth/materialdesignsamples/MainActivity.java
+++ app/src/main/java/com/example/prashanth/materialdesignsamples/MainActivity.java
@@ -10,12 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
-import org.androidannotations.annotations.Click;
 
 
-@EActivity(R.layout.activity_main)
+
+
 public class MainActivity extends ActionBarActivity {
 
     //First We Declare Titles And Icons For Our Navigation Drawer List View
@@ -46,7 +44,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
 
     /* Assinging the toolbar object ot the view
     and setting the the Action bar to our toolbar
